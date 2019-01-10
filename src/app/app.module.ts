25e80bd2563c68369263bd651c716063cb868bf7
Tasos Tsoukalas <tasostsouk.tt2@gmail.com>
2019-01-10 05:17:56 +0200

    Imported angular material library and created the character card.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,25 @@ import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { routeConfig } from "./nav-bar/routeConfig";
+import { CharactersComponent } from "./characters/characters.component";
+import { MatCardModule, MatListModule } from "@angular/material";
+import { MatDividerModule } from "@angular/material/divider";
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, HomeComponent, AboutComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routeConfig)],
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    AboutComponent,
+    CharactersComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routeConfig),
+    MatCardModule,
+    MatDividerModule,
+    MatListModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
