a0ede68ea517fa4a420e1f5f32914292d0fc67d1
chris t <ctselepidas@gmail.com>
2019-01-12 21:43:46 +0100

    RAM-branchName added default route and page not found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { routeConfig } from "./nav-bar/routeConfig";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, HomeComponent, AboutComponent],
+  declarations: [AppComponent, NavBarComponent, HomeComponent, AboutComponent, PageNotFoundComponent],
   imports: [BrowserModule, RouterModule.forRoot(routeConfig)],
   providers: [],
   bootstrap: [AppComponent]
