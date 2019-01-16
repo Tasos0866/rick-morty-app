27390efdbe80069d9e5d0d9f53785afc8a23e7de
Tasos Tsoukalas <tasostsouk.tt2@gmail.com>
2019-01-16 20:11:55 +0200

    homePage : Display the correct character information from the API in the cards and home page styling changes (display four character cards per line).

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,5 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
-
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-characters",
@@ -7,10 +6,9 @@ import {Component, Input, OnInit} from '@angular/core';
   templateUrl: "./characters.component.html"
 })
 export class CharactersComponent implements OnInit {
-  @Input('character') character: Character;
+  @Input("character") character: Character;
 
   constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
